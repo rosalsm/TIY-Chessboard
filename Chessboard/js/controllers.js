5d547d79c43ec917d5b7ea980cff7c424395e2f4
rosa <r215130@gmail.com>
2015-09-27 19:56:59 -0400

    work

--- Chessboard/js/controllers.js
+++ Chessboard/js/controllers.js
@@ -3,10 +3,35 @@
    * Your pattern for Controller code:
    *
    * 1. What `TYPE` of interaction am I interested in? event
+   * The event to listen will be "click" in one of the buttons on the chessboard.
    * 2. What `SELECTOR` will get the element involved in the interaction? element to target.
+   * The element to target will be the one with value number and letter for the next move.
    * 3. What `CALLBACK` should be run when the interaction happens? function to run
+   * The function will move the piece located in the starting point to the first move.
    * 4. What should my `CALLBACK` do with it's `EVENT`...?
+   * When the event happens the callback should listen to the event in the trigger element (button)
+   * and move the piece to the next position.
+    *
+    * event=click
+    *selector=button
+    *callback=function
+    *
    */
+
+jQuery('i.fa fa-play').on('click', nextMove);
+ function nextMove(event){
+   var board = document.querySelector('tr.first-last-row');
+   //buttonPlay will be element 'i.fa fa-play'
+   //this is for the first row but we can do the same with
+   //the rest of them. It is a group of 8 arrays with 8 data cells each.
+   board [tr][td]=board[tr][td] next move?????
+   //after we have the next move, we can change the piece from the satrting point
+   //to the next move.
+ }
+
+
+
+
   document.querySelector(SELECTOR) //SELECTOR is the element we are targetting
     .addEventListener(TYPE, CALLBACK);//TYPE is the event and CALLBACK is the function
   // AKA
@@ -18,19 +43,22 @@
   }
 
 
-  // Controller for "next move"...
+  // Controller for "next move"... This will be play
   jQuery(/* your "next move" button */).on('click', function(event){
     // TODO: Fire tracer bullet!
     // TODO: Tell the Model -- `game` -- to advance to the next move...
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
-  });
+  });//the one that I explain above
 
-  // Controller for "previous move"...
+  // Controller for "previous move"...This will be step-backward
   jQuery(/* your "previous move" button */).on('click', function(event){
     // TODO: Fire tracer bullet!
     // TODO: Tell the Model -- `game` -- to advance to the previous move...
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
   });
+  jQuery('i.fa fa-step-backward').on('click', function(event)){
+    again playing with this board [tr][td];????????
+  }
 
   // Controller for "fast-forward"...
   jQuery(/* your "fast-forward" button */).on(/* wat? */, function(event){

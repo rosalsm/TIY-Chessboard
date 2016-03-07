8032c6ba89e4a796c104e62bd605d5c08bbdff9a
rosa <r215130@gmail.com>
2016-03-07 10:32:02 -0500

    cleaning js files

--- Chessboard/js/controllers.js
+++ Chessboard/js/controllers.js
@@ -1,113 +1,64 @@
-(function(globals){ // What is this even? Do I need it?
-  /**
-   * Your pattern for Controller code:
-   *
-   * 1. What `TYPE` of interaction am I interested in? event
-   * The event to listen will be "click" in one of the buttons on the chessboard.
-   * 2. What `SELECTOR` will get the element involved in the interaction? element to target.
-   * The element to target will be the one with value number and letter for the next move.
-   * 3. What `CALLBACK` should be run when the interaction happens? function to run
-   * The function will move the piece located in the starting point to the first move.
-   * 4. What should my `CALLBACK` do with it's `EVENT`...?
-   * When the event happens the callback should listen to the event in the trigger element (button)
-   * and move the piece to the next position.
-    *
-    * event = click
-    *selector = button
-    *callback = function
-    *
-   */
+(function(globals){
+
 console.log("probando controllers");
 
-// var interv;
-//
-// function playDelay(){
-//   interv = setInterval( playD , 2000);
-// };
-//
-// function playD(){
-//   game.play();
-//   view.helper();
-// }
-//
-// jQuery('.play').on('click', function (event){
-//   console.log("i am in controllers play");
-//    playDelay();
-//  });//end button play on (click)
 
-
-
-jQuery('.play').on('click', function (event){
-  console.log("i am in controllers play");
-   game.play();
-   view.helper();
- });//end button play on (click)
-
-  //  board [tr][td]=board[tr][td] next move?????
-   //after we have the next move, we can change the piece from the starting point
-   //to the next move.
-
-
-  // document.querySelector(SELECTOR) //SELECTOR is the element we are targetting
-  //   .addEventListener(TYPE, CALLBACK);//TYPE is the event and CALLBACK is the function
-  // // AKA
-  // jQuery(SELECTOR).on(TYPE, CALLBACK);
-  // // Where CALLBACK is...
-  // function CALLBACK (EVENT){
-
-
-  // Controller for "next move"... This will be play
+  // Controller for next move.
   jQuery(".next").on('click', function(event){
     console.log('i am in controllers forward');
-    game.next();
-    view.helper();
+    game.next(); //update the model
+    view.helper(); //update the view
     console.log(game.tracer());
-
   });
 
-    // TODO: Fire tracer bullet!
-    // TODO: Tell the Model -- `game` -- to advance to the next move...
-    // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
-
-
-  // Controller for "previous move"...This will be step-backward
+  // Controller for previous move.
   jQuery(".prev").on('click', function(event){
-    game.prev();
-    view.helper();
+    game.prev(); //update the model
+    view.helper(); //update the view
     console.log("i am in controllers and prev")
     console.log(game.tracer());
   });
 
 
-
-    // TODO: Fire tracer bullet!
-    // TODO: Tell the Model -- `game` -- to advance to the previous move...
-    // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
-  //});
-  // jQuery('i.fa fa-step-backward').on('click', function(event)){
-  //   again playing with this board [tr][td];????????
-  // }
-
-  // Controller for "fast-forward"...
+  // Controller for end of moves.
   jQuery(".end").on('click', function(event){
-    game.end();
-    view.helper();
+    game.end(); //update the model
+    view.helper(); //update the view
     console.log("i am in controllers and end")
     console.log(game.tracer());
-    });
-    // TODO: Fire tracer bullet!
-    // TODO: Tell the Model -- `game` -- to advance to the last move...
-    // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
+  });
 
 
-  // Controller for anything else...
+
+  // Controller for restart the chessboard.
    jQuery(".reset").on('click', function(event){
-     game.reset();
-     view.helper();
+     game.reset(); //update the model
+     view.helper(); //update the view
      console.log(game.tracer());
-    // TODO: Fire tracer bullet!
-    // TODO: Tell the Model -- `game` -- to do something it knows how to do...
-    // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
-    });
+   });
+
+   // var interv;
+   //
+   // function playDelay(){
+   //   interv = setInterval( playD , 2000);
+   // };
+   //
+   // function playD(){
+   //   game.play();
+   //   view.helper();
+   // }
+   //
+   // jQuery('.play').on('click', function (event){
+   //   console.log("i am in controllers play");
+   //    playDelay();
+   //  });//end button play on (click)
+
+
+
+   // jQuery('.play').on('click', function (event){
+   //   console.log("i am in controllers play");
+   //    game.play();
+   //    view.helper();
+   //  });//end button play on (click)
 
 })(window || module && module.exports || this)
